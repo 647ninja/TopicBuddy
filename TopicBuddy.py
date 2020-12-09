8a0893c15cf4143452895a1a9b09ddc63819955a
647ninja <647ninja@pm.me>
2020-12-09 04:19:25 -0500

    first commit

---
+++ TopicBuddy.py
@@ -0,0 +1,65 @@
+# Topic Buddy, Cybersecurity topic study Buddy
+# R.
+# 2020-12-09;4:00
+# Required apps countdown-timer
+
+import random
+from countdown import countdown
+
+a = ["Basic Exploitation",
+"Post Exploitation",
+"Service Exploitation",
+"Privilege Escalation",
+"Maintaining Access",
+"Linux Basics",
+"Reconnaissance",
+"Exploitation"
+"Pivoting",
+"MITRE ATTACK",
+"Exploit Research",
+"Web Application Basics",
+"Tools of the Trade",
+"OWASP Top 10",
+"Webapp CVEs",
+"Windows",
+"Linux",
+"Intro to Containers",
+"Container/Host Security",
+"Docker Image Security",
+"Miscellaneous",
+"Basics",
+"Personal Networks",
+"Enterprise Networks",
+"Advanced Labs",
+"Forensics",
+"Hashes",
+"File Passwords",
+"Network Recon",
+"Real World WebApps",
+"Traffic Analysis",
+"Metasploit",
+"Offensive Python",
+"Network Pivoting",
+"Infrastructure Attacks",
+"Firmware Analysis",
+"Reverse Engineering",
+"Secure Coding",
+"Malware Analysis",
+"DevSecOps",
+"Code Repositories",
+"Endpoint Security",
+"Persistence",
+"Linux Runtime Analysis",
+"Cloud Services",
+"REST",
+"MITRE ATTACK Linux",
+"Android Pentesting",
+"DevOps Basics",
+"DevSecOps Basics",
+"Pipeline Basics",
+"Capture the Flags"]
+
+print("Let's study, how about?:",random.choice(a)),
+input("Press Enter to Start timer")
+countdown(1),
+print("Finished!, Well done, have a Good Day!")
